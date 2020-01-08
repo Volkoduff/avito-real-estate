81a3d63c1eb6fbf186d9b4d76c242fb89599911a
Kovalkin Vladimir <vakovalkin@yandex.ru>
2020-01-08 23:36:34 +0300

    Первый коммит

---
+++ src/js/components/app-controller.js
@@ -0,0 +1,86 @@
+import API from './api';
+import Gallery from './main-list';
+import MainList from './main-list';
+import Comment from './comment';
+import {render, unrender} from "./utils";
+
+const LOADING_TEXT = `Загрузка...`;
+const END_POINT = `http://134.209.138.34/items`;
+const api = new API({endPoint: END_POINT});
+const header = document.querySelector(`.header`);
+const body = document.querySelector(`body`);
+
+
+export default class appController {
+    init() {
+        this.mainList = new MainList();
+        // this._renderLoadingText();
+        api.getList()
+            .then((listItems) => {
+                this.mainList.init(listItems);
+                console.log(listItems)
+            })
+            // .then(() => {
+            //     this._unRenderLoadingText();
+            // })
+            // .then(() => this._modalRenderInit())
+    }
+
+    // _modalRenderInit() {
+    //     [...this.gallary.getElement().children]
+    //         .forEach((el) => el.addEventListener(`click`, (evt) => this._onClickRender(evt)))
+    // }
+    //
+    // _onClickRender(evt) {
+    //     const id = evt.target.id;
+    //     api.getComments(id)
+    //         .then((bigPhoto) => this._renderPhotoWithComments(bigPhoto, id));
+    // }
+    //
+    // _renderPhotoWithComments(data, id) {
+    //     body.classList.add(`hidden-overflow`);
+    //     modal.init(data);
+    //     render(header, modal.getElement());
+    //     this.form = modal.getElement().querySelector(`.form`);
+    //     this.form.addEventListener(`submit`, (evt) => this._onSubmit(evt, id))
+    // }
+    //
+    // _onSubmit(evt, id) {
+    //     evt.preventDefault();
+    //     api.addNewComment(id, this._getFormData())
+    //         .then(() => this._renderNewComment())
+    //         .then(() => {
+    //             this.form.reset();
+    //         })
+    // }
+    //
+    // _renderNewComment() {
+    //     const emptyComment = modal.getElement().querySelector(`.empty-comment`);
+    //     if (emptyComment) {
+    //         emptyComment.innerHTML = ``;
+    //     }
+    //     const comment = new Comment(this._getFormData());
+    //     const comments = modal.getElement().querySelector(`.comments`);
+    //     render(comments, comment.getElement())
+    // }
+    //
+    // _renderLoadingText() {
+    //     this.loadingText = document.createElement(`div`);
+    //     this.loadingText.classList.add(`loading`);
+    //     this.loadingText.textContent = LOADING_TEXT;
+    //     header.querySelector(`h1`).after(this.loadingText);
+    // }
+    //
+    // _unRenderLoadingText() {
+    //     unrender(this.loadingText)
+    // }
+    //
+    // _getFormData() {
+    //     let form = new FormData(modal.getElement().querySelector(`form`));
+    //     const formData = {};
+    //     formData.name = form.get(`Name`);
+    //     formData.comment = form.get(`Comment`);
+    //     formData.date = Date();
+    //     return formData;
+    // }
+}
---
+++ src/js/components/main-list.js
@@ -0,0 +1,19 @@
+import BaseComponent from './base-component';
+import {render} from "./utils";
+
+const mainTitle = document.querySelector(`.header h1`);
+
+export default class MainList extends BaseComponent {
+    constructor() {
+        super();
+    }
+
+    init(photos) {
+        this.photos = photos;
+        render(mainTitle, this.getElement());
+    }
+
+    getTemplate() {
+        return `<div class="gallery">${this.photos.map((photo) => `<img src="${photo.url}" id="${photo.id}" alt="Photo id№${photo.id}">`).join(``)}</div>`
+    }
+}
